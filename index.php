41c29616a65e555a34e122ca6a204bcfc0951f16
FaisalDimas <42831478+FaisalDimas@users.noreply.github.com>
2020-11-01 11:16:25 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION["login"])) {
+        header("Location: login.php");
+        exit;
+    }
+
+    require 'functions.php';
+    $mahasiswa = query("SELECT * FROM mahasiswa");
+
+    // tombol cari ditekan
+    if (isset($_POST["cari"])) {
+        $mahasiswa = cari($_POST["keyword"]);
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+    <style>
+        .loader{
+            width:75px;
+            position:absolute;
+            top:110px;
+            left:210px;
+            z-index:-1;
+            display:none;
+        }
+    </style>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+    <br><br>
+
+    <form action="" method="post">
+        <input type="text" name="keyword" id="keyword" size="25" placeholder="Masukkan keyword pencarian" autocomplete="off" autofocus>
+        <button type="submit" name="cari" id="tombol-cari">Cari</button>
+        <img src="img/loader.gif" class="loader">
+    </form>
+    <br>
+
+    <div id="container">
+        <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Nama</th>
+            <th>NRP</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+        <?php $i = 1; ?>
+        <?php foreach($mahasiswa as $row) : ?>
+        <tr>
+            <td><?= $i; ?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a> |
+                <a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Yakin?'); ">Hapus</a>
+            </td>
+            <td><img src="img/<?= $row["gambar"]; ?>" width="50"></td>
+            <td><?= $row["nama"]; ?></td>
+            <td><?= $row["nrp"]; ?></td>
+            <td><?= $row["email"]; ?></td>
+            <td><?= $row["jurusan"]; ?></td>
+        </tr>
+        <?php $i++; ?>
+        <?php endforeach; ?>
+        </table>
+    </div>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
---
+++ phpdasar.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Nov 2020 pada 05.11
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpdasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nrp` char(9) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALUES
+(15, 'piyan', 'ads', 'adsa', 'dsa', '1.jpg'),
+(16, 'ASD', 'ASD', 'ASD', 'ASD', '2.JPG'),
+(17, 'asd', 'asd', 'asd', 'ads', '5f953509b84a6.jpg'),
+(18, 'asd', 'asd', 'asd', 'asd', '2.jpg'),
+(19, 'asd', 'asd', 'ad', 'asd', '5f95321ce86b1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'piyan', '$2y$10$Fh4gRzF71xwXMikAkAgRUOeL4CAYJttCgvNqLhuEO8p0VJWWlcsQu');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
